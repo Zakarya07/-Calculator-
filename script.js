aebc4ae9c61325d6682985a3ba26baa00dfe2701
Zakarya <zakarya.elkhattabi@gmail.com>
2020-03-23 02:10:24 +0100

    Modification des messages des fonctions de calculs(alert) && lèger redesign

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
+//------------------------------------------Function Multiplication
 function print_result_multiplication() {
 
-    let number_multiply_1 = prompt(`Enter your first number to multiply`, '');
+    let number_multiply_1 = prompt(`Entrez un premier chiffre pour multiplier `, '');
     if (number_multiply_1 == '' || number_multiply_1 == null) {
 
         multiply() = undefined;
     }
-    let number_multiply_2 = prompt(`Enter your second number to multiply`, '');
-
-
+    let number_multiply_2 = prompt(`Entrez un second chiffre pour multiplier `, '');
 
     function multiply(a, b) {
         return a * b;
-
     }
 
     multiply();
-    alert(`The result is : ${multiply(number_multiply_1,number_multiply_2)}`);
-
-
+    alert(`Le résultat de la multiplication est: ${multiply(number_multiply_1,number_multiply_2)}`);
 }
 
-
+//------------------------------------------Function Addition
 function print_result_addition() {
 
-    let number_addition_1 = prompt(`Enter your first number to additionnates`, '');
+    let number_addition_1 = prompt(`Entrez un premier chiffre pour additioner`, '');
     if (number_addition_1 == '' || number_addition_1 == null) {
 
         addition() = undefined;
     }
 
-    let number_addition_2 = prompt(`Enter your second number to additionnates`, '');
+    let number_addition_2 = prompt(`Entrez un second chiffre pour additionner`, '');
 
     function addition(a, b) {
         return +a + +b;
@@ -37,21 +33,21 @@ function print_result_addition() {
     }
 
     addition();
-    alert(`The result is : ${addition(number_addition_1,number_addition_2)}`);
+    alert(`Le résulat de l'addition est: ${addition(number_addition_1,number_addition_2)}`);
 
 
 }
 
-
+//------------------------------------------Function Soustraction
 function print_result_substraction() {
 
-    let number_substract_1 = prompt(`Enter your first number to substractes`, '');
+    let number_substract_1 = prompt(`Entrez un premier chiffre pour soustraire`, '');
     if (number_substract_1 == '' || number_substract_1 == null) {
 
         substraction() = undefined;
     }
 
-    let number_substract_2 = prompt(`Enter your second number to substractes`, '');
+    let number_substract_2 = prompt(`Entrez un second chiffre pour soustraire`, '');
 
     function substraction(a, b) {
         return a - b;
@@ -59,19 +55,18 @@ function print_result_substraction() {
     }
 
     substraction();
-    alert(`The result is : ${substraction(number_substract_1,number_substract_2)}`);
-
-
+    alert(`Le résultat de la soustraction est: ${substraction(number_substract_1,number_substract_2)}`);
 }
 
+//------------------------------------------Function Division
 function print_result_division() {
 
-    let number_divided_1 = prompt(`Enter your first number to divides`, '');
+    let number_divided_1 = prompt(`Entrez un premier chiffre pour diviser`, '');
     if (number_divided_1 == '' || number_divided_1 == null) {
 
         division() = undefined;
     }
-    let number_divided_2 = prompt(`Enter your second number to divides`, '');
+    let number_divided_2 = prompt(`Entrez un premier chiffre pour diviser`, '');
 
     function division(a, b) {
         return a / b;
@@ -79,54 +74,51 @@ function print_result_division() {
     }
 
     division();
-    alert(`The result is : ${division(number_divided_1,number_divided_2)}`);
-
+    alert(`Le résultat de la division est: ${division(number_divided_1,number_divided_2)}`);
 
 }
 
+//------------------------------------------Function Reste (Modulo)
 function print_result_remainder() {
 
-    let number_remainder_1 = prompt(`Enter your first number to remaind`, '');
+    let number_remainder_1 = prompt(`Entrez un premier chiffre pour calculer le reste`, '');
     if (number_remainder_1 == '' || number_remainder_1 == null) {
 
         remainder() = undefined;
     }
-    let number_remainder_2 = prompt(`Enter your second number to remaind`, '');
+    let number_remainder_2 = prompt(`Entrez un second chiffre pour calculer le reste`, '');
 
     function remainder(a, b) {
         return a % b;
-
     }
 
     remainder();
-    alert(`The result is : ${remainder(number_remainder_1,number_remainder_2)}`);
+    alert(`Le résultat du modulo est: ${remainder(number_remainder_1,number_remainder_2)}`);
 }
 
+//------------------------------------------Function Puissance
 function print_result_exponentiation() {
 
-    let number_exponentiation_1 = prompt(`Enter your first number to exponent`, '');
+    let number_exponentiation_1 = prompt(`Entrez un premier chiffre pour trouver la puissance`, '');
     if (number_exponentiation_1 == '' || number_exponentiation_1 == null) {
 
         exponentiation() = undefined;
     }
-    let number_exponentiation_2 = prompt(`Enter your second number to exponent`, '');
-
+    let number_exponentiation_2 = prompt(`Entrez un second chiffre pour trouver la puissance`, '');
 
     function exponentiation(a, b) {
         return a ** b;
-
-
     }
 
     exponentiation();
-    alert(`The result is : ${exponentiation(number_exponentiation_1,number_exponentiation_2)}`);
-
+    alert(`Le résulat de ${number_exponentiation_1} exposant ${number_exponentiation_2} est: ${exponentiation(number_exponentiation_1,number_exponentiation_2)}`);
 
 }
 
+//------------------------------------------Function Carré d'un nombre
 function print_result_square() {
 
-    let number_square = prompt(`Enter your number to find your square`, '');
+    let number_square = prompt(`Entrez un nombre pour trouver le carré de votre chiffre`, '');
     if (number_square == '' || number_square == null) {
 
         square() = undefined;
@@ -138,23 +130,11 @@ function print_result_square() {
     }
 
     square();
-    alert(`The result is : ${square(number_square)}`);
-
-
+    alert(`Le carré de ${number_square} est: ${square(number_square)}`);
 }
 
-function hour() {
-    let hour = prompt('Heure', '');
-    let minute = prompt('Minute ', '');
-
-    if (hour == hour && minute == minute) {
-        alert(`it's ${hour}:${minute}`)
-    }
-}
-
-
-
-function auth() {
+// TESTING  FOR IMPROVING SKILLS WITH AN ''SIGN IN LOGICAL FUNCTION''
+function authentification() {
 
     let idendity = prompt('Choose a username', '');
     let passwordAccount = prompt(`Choose a password`)
@@ -179,6 +159,3 @@ function auth() {
 
 
 }
-
-let i = 0;
-while (i) alert(i--);
