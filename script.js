1c3610645552366e7702ea09266609ed7188863b
Zakarya <zakarya.elkhattabi@gmail.com>
2020-03-24 00:49:52 +0100

    Création  fonction 'resultOfCurrentOperation' pour afficher le résultat avec InnerHTML et léger CSS

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 //------------------------------------------Function Multiplication
 function print_result_multiplication() {
 
-    let number_multiply_1 = prompt(`Entrez un premier chiffre pour multiplier `, '');
+    let number_multiply_1 = Number(prompt(`Entrez un premier chiffre pour multiplier `, ''));
     if (number_multiply_1 == '' || number_multiply_1 == null) {
 
         multiply() = undefined;
     }
-    let number_multiply_2 = prompt(`Entrez un second chiffre pour multiplier `, '');
+    let number_multiply_2 = Number(prompt(`Entrez un second chiffre pour multiplier `, ''));
 
     function multiply(a, b) {
         return a * b;
     }
 
     multiply();
-    alert(`Le résultat de la multiplication est: ${multiply(number_multiply_1,number_multiply_2)}`);
+    resultOfCurrentOperation(`Le résultat de la multiplication est: ${multiply(number_multiply_1,number_multiply_2)}`);
 }
 
 //------------------------------------------Function Addition
 function print_result_addition() {
 
-    let number_addition_1 = prompt(`Entrez un premier chiffre pour additioner`, '');
+    let number_addition_1 = Number(prompt(`Entrez un premier chiffre pour additioner`, ''));
     if (number_addition_1 == '' || number_addition_1 == null) {
 
         addition() = undefined;
     }
 
-    let number_addition_2 = prompt(`Entrez un second chiffre pour additionner`, '');
+    let number_addition_2 = Number(prompt(`Entrez un second chiffre pour additionner`, ''));
 
     function addition(a, b) {
         return +a + +b;
@@ -33,7 +33,7 @@ function print_result_addition() {
     }
 
     addition();
-    alert(`Le résulat de l'addition est: ${addition(number_addition_1,number_addition_2)}`);
+    resultOfCurrentOperation(`Le résulat de l'addition est: ${addition(number_addition_1,number_addition_2)}`);
 
 
 }
@@ -41,13 +41,13 @@ function print_result_addition() {
 //------------------------------------------Function Soustraction
 function print_result_substraction() {
 
-    let number_substract_1 = prompt(`Entrez un premier chiffre pour soustraire`, '');
+    let number_substract_1 = Number(prompt(`Entrez un premier chiffre pour soustraire`, ''));
     if (number_substract_1 == '' || number_substract_1 == null) {
 
         substraction() = undefined;
     }
 
-    let number_substract_2 = prompt(`Entrez un second chiffre pour soustraire`, '');
+    let number_substract_2 = Number(prompt(`Entrez un second chiffre pour soustraire`, ''));
 
     function substraction(a, b) {
         return a - b;
@@ -55,18 +55,18 @@ function print_result_substraction() {
     }
 
     substraction();
-    alert(`Le résultat de la soustraction est: ${substraction(number_substract_1,number_substract_2)}`);
+    resultOfCurrentOperation(`Le résultat de la soustraction est: ${substraction(number_substract_1,number_substract_2)}`);
 }
 
 //------------------------------------------Function Division
 function print_result_division() {
 
-    let number_divided_1 = prompt(`Entrez un premier chiffre pour diviser`, '');
+    let number_divided_1 = Number(prompt(`Entrez un premier chiffre pour diviser`, ''));
     if (number_divided_1 == '' || number_divided_1 == null) {
 
         division() = undefined;
     }
-    let number_divided_2 = prompt(`Entrez un premier chiffre pour diviser`, '');
+    let number_divided_2 = Number(prompt(`Entrez un premier chiffre pour diviser`, ''));
 
     function division(a, b) {
         return a / b;
@@ -74,51 +74,51 @@ function print_result_division() {
     }
 
     division();
-    alert(`Le résultat de la division est: ${division(number_divided_1,number_divided_2)}`);
+    resultOfCurrentOperation(`Le résultat de la division est: ${division(number_divided_1,number_divided_2)}`);
 
 }
 
 //------------------------------------------Function Reste (Modulo)
 function print_result_remainder() {
 
-    let number_remainder_1 = prompt(`Entrez un premier chiffre pour calculer le reste`, '');
+    let number_remainder_1 =Number(prompt(`Entrez un premier chiffre pour calculer le reste`, ''));
     if (number_remainder_1 == '' || number_remainder_1 == null) {
 
         remainder() = undefined;
     }
-    let number_remainder_2 = prompt(`Entrez un second chiffre pour calculer le reste`, '');
+    let number_remainder_2 = Number(prompt(`Entrez un second chiffre pour calculer le reste`, ''));
 
     function remainder(a, b) {
         return a % b;
     }
 
     remainder();
-    alert(`Le résultat du modulo est: ${remainder(number_remainder_1,number_remainder_2)}`);
+    resultOfCurrentOperation(`Le résultat du modulo est: ${remainder(number_remainder_1,number_remainder_2)}`);
 }
 
 //------------------------------------------Function Puissance
 function print_result_exponentiation() {
 
-    let number_exponentiation_1 = prompt(`Entrez un premier chiffre pour trouver la puissance`, '');
+    let number_exponentiation_1 = Number(prompt(`Entrez un premier chiffre pour trouver la puissance`, ''));
     if (number_exponentiation_1 == '' || number_exponentiation_1 == null) {
 
         exponentiation() = undefined;
     }
-    let number_exponentiation_2 = prompt(`Entrez un second chiffre pour trouver la puissance`, '');
+    let number_exponentiation_2 = Number(prompt(`Entrez un second chiffre pour trouver la puissance`, ''));
 
     function exponentiation(a, b) {
         return a ** b;
     }
 
     exponentiation();
-    alert(`Le résulat de ${number_exponentiation_1} exposant ${number_exponentiation_2} est: ${exponentiation(number_exponentiation_1,number_exponentiation_2)}`);
+    resultOfCurrentOperation(`Le résulat  de ${number_exponentiation_1} exposant ${number_exponentiation_2} est: ${exponentiation(number_exponentiation_1,number_exponentiation_2)}`);
 
 }
 
 //------------------------------------------Function Carré d'un nombre
 function print_result_square() {
 
-    let number_square = prompt(`Entrez un nombre pour trouver le carré de votre chiffre`, '');
+    let number_square = Number(prompt(`Entrez un nombre pour trouver le carré de votre chiffre`, ''));
     if (number_square == '' || number_square == null) {
 
         square() = undefined;
@@ -130,32 +130,38 @@ function print_result_square() {
     }
 
     square();
-    alert(`Le carré de ${number_square} est: ${square(number_square)}`);
+    resultOfCurrentOperation(`Le carré de ${number_square} est: ${square(number_square)}`);
 }
 
-// TESTING  FOR IMPROVING SKILLS WITH AN ''SIGN IN LOGICAL FUNCTION''
-function authentification() {
-
-    let idendity = prompt('Choose a username', '');
-    let passwordAccount = prompt(`Choose a password`)
-
-    let user = prompt('Username ?', '');
-
-    if (user == idendity) {
-        let password = prompt('Password ?');
-
-        if (password == passwordAccount) {
-            alert(`Welcome to ElkhattaCorp website ''${user.toUpperCase()}'', your account has been created succefully !  ;)`);
-
-        } else if (password !== passwordAccount || password == null) {
-            alert('Wrong Password !')
-        }
-
-    } else if (user == null || user !== idendity) {
-        alert('Canceled')
-    } else {
-        alert(`"${user.toUpperCase()}" is not registred`)
-    }
 
 
-}
+function resultOfCurrentOperation(currentOperation) 
+      {
+        document.getElementById("final_result").innerHTML=`${currentOperation}`;
+      }
+
+
+
+// let gett= document.getElementById("g-p");
+
+// function growThings(items){
+
+//     let elemWithId= document.getElementById(`${items}`);
+//     let elemWithClass= document.getElementsByClassName(`${items}`);
+//     let elemWithTag=document.getElementsByTagName(`${items}`);
+
+//     if (elemWithId) {
+//         elemWithId.style.color="yellow";
+//         }
+//     else if(elemWithClass){
+//         elemWithClass.style.color="yellow";
+
+//     }
+//     else if(elemWithTag){
+
+//         elemWithTag.style.color="yellow";
+//     }
+
+// }
+
+// growThings(gett);
